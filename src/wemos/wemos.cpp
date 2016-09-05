0cc73db8ede56d3c7b55eaac8ad81008c8e77642
Sven Dowideit <SvenDowideit@home.org.au>
2016-09-05 13:07:53 +1000

    pring mac address too
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- src/wemos/wemos.cpp
+++ src/wemos/wemos.cpp
@@ -29,4 +29,19 @@ void setupWIFI(const char *ssid, const char *password) {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+
+ byte mac[6];                     // the MAC address of your Wifi shield
+  WiFi.macAddress(mac);
+  Serial.print("MAC: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
 }
